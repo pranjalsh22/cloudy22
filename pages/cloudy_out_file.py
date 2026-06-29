baf5c5c1d9d62d5e277496cdee83c0f92b829c4d
Pranjal Sharma <physics0403@gmail.com>
2026-06-29 21:15:07 +0530

    Update cloudy_out_file.py

--- pages/cloudy_out_file.py
+++ pages/cloudy_out_file.py
@@ -1,4 +1,5 @@
-#version6
+#version7
+
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -54,6 +55,19 @@ def find_warnings(file_content):
 def final_iteration_content(content):
     #st.text_area(content)
     final_iter_match = re.search(r'Cloudy ends:.*?(\d+)\s+iterations?', content)
+
+
+    if final_iter_match:
+
+        st.info(f"Number of iterations: {final_iter_match.group(1)}")
+
+    else:
+
+        st.error("Could not determine number of iterations.")
+
+        return None
+
+    
     st.info(f"Number of iterations:{final_iter_match[1]}")
     if final_iter_match:
         final_iteration = int(final_iter_match.group(1))
@@ -84,185 +98,280 @@ def extract_emergent_lines(content):
         else:
             st.warning("Could not find 'Emergent line intensities' block.")
             return ""
+def is_main_line(label, wavelength, main_lines, main_wavelengths, tol=0.5):
+    label = label.strip().lower()
 
+    label_match = any(x in label for x in main_lines)
+
+    wavelength_match = any(
+        abs(wavelength - w) <= tol
+        for w in main_wavelengths
+    )
+
+    return label_match or wavelength_match
 #-----------------------------APP START--------------------------------------------
 st.title("Cloudy Output File Processor")
 st.write("Upload a Cloudy output file to extract and visualize line luminosities, and explore other results.")
 
-uploaded_file = st.file_uploader("Upload Cloudy Output File", type=["txt","out"])
+uploaded_files = st.file_uploader(
+    "Upload Cloudy Output Files",
+    type=["txt", "out"],
+    accept_multiple_files=True)
 
-main_lines=["o 3","o3bn"]
+st.subheader("Global Analysis Settings")
 
-if uploaded_file:
-    file_content = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
-    warnings=find_warnings(file_content)
-    content1=final_iteration_content(file_content)
-    content=extract_emergent_lines(content1)
-    wavelengths, luminosities, labels= extract_cloudy_data(content)
-    # Create a DataFrame for the line data
-    line_data = pd.DataFrame({
-        "Label": labels,
-        "Wavelength(Å)": wavelengths,
-        "luminosity(erg/s)": luminosities,
-        #"Relative luminosity": rel_luminosities
-    })
-    # Display Input Parameters at the Start
-    st.subheader("Input Parameters")
-    st.write("### Total Emission Lines Extracted:", len(wavelengths))
-    if st.checkbox("see warnings"):
-        st.write("### Warnings:")
-        st.write("\n \n".join(warnings) if warnings else "No additional results found.")
+# ---------------- Highlight labels ----------------
 
-#--------------------GRAPH 1: ONLY MAIN LINES------------------------------
-    st.subheader("Main Emission Lines Only")
-    main_wavelengths = [4363,4363.21,4958.91,5006.84,4931.23,5007]  # example wavelengths in Å
-    st.write(f"looking for wavelengths(Å) :{main_wavelengths}")
-    main_data = line_data[line_data["Wavelength(Å)"].isin(main_wavelengths)]
-    st.dataframe(main_data,use_container_width=True)
-    
-    if not main_data.empty:
-        fig, ax = plt.subplots(figsize=(8, 4))
-        ax.bar(
-            main_data["Wavelength(Å)"],
-            main_data["luminosity(erg/s)"],
-            width=5.0,
-            color="green",
-            edgecolor="black",
-            linewidth=0.3,
-            alpha=0.8
-        )
+main_lines_input = st.text_input("Emission line labels to highlight (comma separated)",value="o 3, o3bn")
 
-        # Annotate each line with its label
-        for _, row in main_data.iterrows():
-            ax.text(row["Wavelength(Å)"], row["luminosity(erg/s)"] + 0.1, row["Label"],
-                    fontsize=10, color="darkgreen")
+main_lines = [
+    line.strip().lower()
+    for line in main_lines_input.split(",")
+    if line.strip()
+]
 
-        ax.set_title("Main Emission Lines", fontsize=16)
-        ax.set_xlabel("Wavelength (Å)", fontsize=14)
-        ax.set_ylabel("Log(luminosity) [erg/s]", fontsize=14)
-        ax.set_ylim(bottom=0)
+# ---------------- Main wavelengths ----------------
 
-        # User input for custom X-axis range
-        st.subheader("Set X-Axis Range for Main Emission Line Plot")
-        col1, col2 = st.columns(2)
+main_wavelength_input = st.text_input(
+    "Main wavelengths (Å, comma separated)",
+    value="4363,5007"
+)
 
-        with col1:
-            min_x = st.number_input("Minimum Wavelength (Å)", value=4200)#int(min(main_data["Wavelength(Å)"])))
+main_wavelengths = [
+    float(x)
+    for x in main_wavelength_input.split(",")
+    if x.strip()
+]
 
-        with col2:
-            max_x = st.number_input("Maximum Wavelength (Å)", value=5100)#int(max(main_data["Wavelength(Å)"])))
+if uploaded_files:
 
-        ax.set_xlim(min_x, max_x)
-        ax.grid(axis="y", linestyle="--", alpha=0.6)
-        st.pyplot(fig)
-    else:
-        st.write("No main emission lines found in the data.")
+    for uploaded_file in uploaded_files:
 
+        with st.expander(f"📄 {uploaded_file.name}", expanded=False):
 
-#--------------------GRAPH 2:FILTERED GRAPH------------------------------
-    # Display the emission lines in a scrollable box
-    st.subheader("Emission Line Strengths")
-    
-    st.dataframe(line_data, height=300, use_container_width=True)
+            file_content = StringIO(
+                uploaded_file.getvalue().decode("utf-8")
+            ).read()
 
-    st.subheader("Filter Emission Lines by Wavelength and Log(luminosity)")
+            warnings = find_warnings(file_content)
+            content1 = final_iteration_content(file_content)
+            content = extract_emergent_lines(content1)
+            wavelengths, luminosities, labels = extract_cloudy_data(content)
 
-    # Input for wavelength range
-    min_wavelength = st.number_input(
-        "Minimum Wavelength (Å)",
-        min_value=int(min(wavelengths)),
-        max_value=int(max(wavelengths)),
-        value=int(min(wavelengths))
-    )
+            line_data = pd.DataFrame({
+                "Label": labels,
+                "Wavelength(Å)": wavelengths,
+                "luminosity(erg/s)": luminosities,
+            })
 
-    max_wavelength = st.number_input(
-        "Maximum Wavelength (Å)",
-        min_value=int(min(wavelengths)),
-        max_value=int(max(wavelengths)),
-        value=int(max(wavelengths))
-    )
-
-    # Input for log luminosity range
-    min_luminosity = st.number_input(
-        "Minimum Log(luminosity)",
-        min_value=int(min(luminosities)),
-        max_value=int(max(luminosities)),
-        value=int(min(luminosities))
-    )
-
-    max_luminosity = st.number_input(
-        "Maximum Log(luminosity)",
-        min_value=int(min(luminosities)),
-        max_value=int(max(luminosities)),
-        value=int(max(luminosities))
-    )
-
-    # Filter the line data based on the selected ranges
-    filtered_data = line_data[
-        (line_data["Wavelength(Å)"] >= min_wavelength) & 
-        (line_data["Wavelength(Å)"] <= max_wavelength) & 
-        (line_data["luminosity(erg/s)"] >= min_luminosity) & 
-        (line_data["luminosity(erg/s)"] <= max_luminosity)
-    ]
-
-    # Display filtered data
-    st.write(f"### Filtered Emission Lines (Wavelength: {min_wavelength} - {max_wavelength} Å, Log(luminosity): {min_luminosity} - {max_luminosity})")
-    if not filtered_data.empty:
-        st.dataframe(filtered_data, height=300, use_container_width = True)
-
-        # Plotting Section: Plot the filtered data
-        st.write("### Plot the Filtered Emission Lines")
-        fig, ax = plt.subplots(figsize=(10, 5))
-        # Plot all bars
-        ax.bar(filtered_data["Wavelength(Å)"], filtered_data["luminosity(erg/s)"],width=1.0,
-    color="blue",
-    edgecolor="black",      # Adds a visible outline
-    linewidth=0.2,          # Controls thickness of bar edges
-    alpha=0.7)        # Highlight and label only "O 3" and "O3bn"
-        for _, row in filtered_data.iterrows():
-            label_clean = row["Label"].strip().lower()
-            if label_clean in main_lines:
-                ax.text(row["Wavelength(Å)"], row["luminosity(erg/s)"] + 0.1, row["Label"],
-                        fontsize=10, color="red")
+            file_content = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
         
-        # Set the y-axis limits to match the user input range
-        ax.set_ylim(0, max_luminosity)
-        
-        ax.set_title("Filtered Emission Line luminosities", fontsize=16)
-        ax.set_xlabel("Wavelength (Å)", fontsize=14)
-        ax.set_ylabel("Log(luminosity)", fontsize=14)
-        ax.grid(axis="y", linestyle="--", alpha=0.6)
-        st.pyplot(fig)
+            # Display Input Parameters at the Start
+            st.subheader("Input Parameters")
+            st.write("### Total Emission Lines Extracted:", len(wavelengths))
+            if st.checkbox("See warnings",key=f"warnings_{uploaded_file.name}"):
+                st.write("### Warnings:")
+                st.write("\n \n".join(warnings) if warnings else "No additional results found.")
 
-        # Option to download filtered data
-        st.download_button(
-            label="Download Filtered Emission Line Data as CSV",
-            data=filtered_data.to_csv(index=False),
-            file_name="filtered_emission_line_data.csv",
-            mime="text/csv"
-        )
-    else:
-        st.write("No emission lines match the selected filters!")
-#--------------------GRAPH 3: ALL LINES------------------------------
-    st.subheader("Full Emission Line luminosities")
-    fig, ax = plt.subplots(figsize=(12, 6))
-    ax.bar(
-    wavelengths,
-    luminosities,
-    width=1.0,
-    color="blue",
-    edgecolor="black",   
-    linewidth=0.2,       
-    alpha=0.7)
-    for wl, lum, label in zip(wavelengths, luminosities, labels):
-        label_clean = label.strip().lower()
-        if any(tag in label_clean for tag in main_lines):
-            ax.text(wl, lum + 0.1, label, fontsize=9, color="red")
+        #--------------------GRAPH 1: ONLY MAIN LINES------------------------------
+            st.subheader("Main Emission Lines Only")
+            #main_wavelengths = [4363,4363.21,4958.91,5006.84,4931.23,5007]  # example wavelengths in Å
+            st.write(f"looking for wavelengths(Å) :{main_wavelengths}")
+            main_data = line_data[
+                line_data.apply(
+                    lambda row: is_main_line(
+                        row["Label"],
+                        row["Wavelength(Å)"],
+                        main_lines,
+                        main_wavelengths
+                    ),
+                    axis=1
+                )
+            ]
+            st.dataframe(main_data,use_container_width=True)
+            
+            if not main_data.empty:
+                fig, ax = plt.subplots(figsize=(8, 4))
+                ax.bar(
+                    main_data["Wavelength(Å)"],
+                    main_data["luminosity(erg/s)"],
+                    width=5.0,
+                    color="green",
+                    edgecolor="black",
+                    linewidth=0.3,
+                    alpha=0.8
+                )
 
-    if st.checkbox("log scale x axis",value=True):
-        ax.set_xscale('log')
-    ax.set_title("Full Emission Line luminosities", fontsize=16)
-    ax.set_xlabel("Wavelength (Å)", fontsize=14)
-    ax.set_ylabel("Log(luminosity) [erg/s]", fontsize=14)
-    ax.grid(axis="y", linestyle="--", alpha=0.6)
-    st.pyplot(fig)
+                # Annotate each line with its label
+                for _, row in main_data.iterrows():
+                    ax.text(row["Wavelength(Å)"], row["luminosity(erg/s)"] + 0.1, row["Label"],
+                            fontsize=10, color="darkgreen")
+
+                ax.set_title("Main Emission Lines", fontsize=16)
+                ax.set_xlabel("Wavelength (Å)", fontsize=14)
+                ax.set_ylabel("Log(luminosity) [erg/s]", fontsize=14)
+                #ax.set_ylim(bottom=0)
+                ax.set_ylim(
+                    min(main_data["luminosity(erg/s)"]) - 1,
+                    max(main_data["luminosity(erg/s)"]) + 1
+                )
+                # User input for custom X-axis range
+                st.subheader("Set X-Axis Range for Main Emission Line Plot")
+                col1, col2 = st.columns(2)
+                default_min_x = int(main_data["Wavelength(Å)"].min()) - 200
+                default_max_x = int(main_data["Wavelength(Å)"].max()) + 200
+
+                with col1:
+
+                    key = f"minx_{uploaded_file.name}"
+
+                    if key not in st.session_state:
+                        st.session_state[key] = default_min_x
+
+                    min_x = st.number_input("Minimum Wavelength (Å)",key=key)
+                    
+
+                with col2:
+                    max_x = st.number_input(
+                        "Maximum Wavelength (Å)",
+                        value=default_max_x,
+                        key=f"maxx_{uploaded_file.name}"
+                    )
+                st.write(main_data["Wavelength(Å)"].min(), main_data["Wavelength(Å)"].max())
+                ax.set_xlim(min_x, max_x)
+                ax.grid(axis="y", linestyle="--", alpha=0.6)
+                
+                st.pyplot(fig)
+            else:
+                st.write("No main emission lines found in the data.")
+
+
+        #--------------------GRAPH 2:FILTERED GRAPH------------------------------
+            # Display the emission lines in a scrollable box
+            st.subheader("Emission Line Strengths")
+            
+            st.dataframe(line_data, height=300, use_container_width=True)
+
+            st.subheader("Filter Emission Lines by Wavelength and Log(luminosity)")
+
+            # Input for wavelength range
+            min_wavelength = st.number_input(
+                "Minimum Wavelength (Å)",
+                min_value=int(min(wavelengths)),
+                max_value=int(max(wavelengths)),
+                value=int(min(wavelengths)),key=f"minwavw_{uploaded_file.name}"
+            )
+
+            max_wavelength = st.number_input(
+                "Maximum Wavelength (Å)",
+                min_value=int(min(wavelengths)),
+                max_value=int(max(wavelengths)),
+                value=int(max(wavelengths)),key=f"maxwavw_{uploaded_file.name}"
+            )
+
+            # Input for log luminosity range
+            min_luminosity = st.number_input(
+                "Minimum Log(luminosity)",
+                min_value=int(min(luminosities)),
+                max_value=int(max(luminosities)),
+                value=int(min(luminosities)),key=f"minlum_{uploaded_file.name}"
+            )
+
+            max_luminosity = st.number_input(
+                "Maximum Log(luminosity)",
+                min_value=int(min(luminosities)),
+                max_value=int(max(luminosities)),
+                value=int(max(luminosities)),key=f"maxlum_{uploaded_file.name}"
+            )
+
+            # Filter the line data based on the selected ranges
+            filtered_data = line_data[
+                (line_data["Wavelength(Å)"] >= min_wavelength) & 
+                (line_data["Wavelength(Å)"] <= max_wavelength) & 
+                (line_data["luminosity(erg/s)"] >= min_luminosity) & 
+                (line_data["luminosity(erg/s)"] <= max_luminosity)
+            ]
+
+            # Display filtered data
+            st.write(f"### Filtered Emission Lines (Wavelength: {min_wavelength} - {max_wavelength} Å, Log(luminosity): {min_luminosity} - {max_luminosity})")
+            if not filtered_data.empty:
+                st.dataframe(filtered_data, height=300, use_container_width = True)
+
+                # Plotting Section: Plot the filtered data
+                st.write("### Plot the Filtered Emission Lines")
+                fig, ax = plt.subplots(figsize=(10, 5))
+                # Plot all bars
+                ax.bar(filtered_data["Wavelength(Å)"], filtered_data["luminosity(erg/s)"],width=1.0,
+            color="blue",
+            edgecolor="black",      # Adds a visible outline
+            linewidth=0.2,          # Controls thickness of bar edges
+            alpha=0.7)        # Highlight and label only "O 3" and "O3bn"
+                for _, row in filtered_data.iterrows():
+
+                    if is_main_line(
+                        row["Label"],
+                        row["Wavelength(Å)"],
+                        main_lines,
+                        main_wavelengths
+                    ):
+                        ax.text(
+                            row["Wavelength(Å)"],
+                            row["luminosity(erg/s)"] + 0.1,
+                            row["Label"],
+                            fontsize=10,
+                            color="red"
+                        )
+
+                    
+                # Set the y-axis limits to match the user input range
+                ax.set_ylim(0, max_luminosity)
+                
+                ax.set_title("Filtered Emission Line luminosities", fontsize=16)
+                ax.set_xlabel("Wavelength (Å)", fontsize=14)
+                ax.set_ylabel("Log(luminosity)", fontsize=14)
+                ax.grid(axis="y", linestyle="--", alpha=0.6)
+                st.pyplot(fig)
+
+                # Option to download filtered data
+                st.download_button(
+                    label="Download Filtered Emission Line Data as CSV",
+                    data=filtered_data.to_csv(index=False),
+                    file_name=f"{uploaded_file.name}_filtered.csv",
+                    mime="text/csv",key=f"download_{uploaded_file.name}"
+                )
+            else:
+                st.write("No emission lines match the selected filters!")
+        #--------------------GRAPH 3: ALL LINES------------------------------
+            st.subheader("Full Emission Line luminosities")
+            fig, ax = plt.subplots(figsize=(12, 6))
+            ax.bar(
+            wavelengths,
+            luminosities,
+            width=1.0,
+            color="blue",
+            edgecolor="black",   
+            linewidth=0.2,       
+            alpha=0.7)
+            for wl, lum, label in zip(wavelengths, luminosities, labels):
+                
+                if is_main_line(
+                    label,
+                    wl,
+                    main_lines,
+                    main_wavelengths
+                ):
+                    ax.text(
+                        wl,
+                        lum + 0.1,
+                        label,
+                        fontsize=9,
+                        color="red"
+                    )
+
+            if st.checkbox("log scale x axis",value=True,key=f"logscale_{uploaded_file.name}"):
+                ax.set_xscale('log')
+            ax.set_title("Full Emission Line luminosities", fontsize=16)
+            ax.set_xlabel("Wavelength (Å)", fontsize=14)
+            ax.set_ylabel("Log(luminosity) [erg/s]", fontsize=14)
+            ax.grid(axis="y", linestyle="--", alpha=0.6)
+            st.pyplot(fig)
